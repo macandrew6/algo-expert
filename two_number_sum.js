ed5d2265cd5fa8eb4952fe3099cd86a485abfcb1
macandrew6 <mr.betterandrewmac@gmail.com>
2020-09-22 22:53:53 -0700

    starting two_number_sum

---
+++ two_number_sum.js
@@ -0,0 +1,9 @@
+function twoNumberSum(array, targetSum) {
+  // Write your code here.
+  console.log(array, targetSum);
+}
+
+let array = [3, 5, -4, 8, 11, 1, -1, 6];
+let targetSum = 10;
+
+twoNumberSum(array, targetSum);
