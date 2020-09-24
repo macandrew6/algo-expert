a29ac7b329e6b8d69f26dc44b432cdd7855dccf1
macandrew6 <mr.betterandrewmac@gmail.com>
2020-09-23 22:17:20 -0700

    doing algos

--- two_number_sum.js
+++ two_number_sum.js
@@ -1,5 +1,6 @@
 function twoNumberSum(array, targetSum) {
   // Write your code here.
+  //
   console.log(array, targetSum);
 }
 
@@ -7,3 +8,5 @@ let array = [3, 5, -4, 8, 11, 1, -1, 6];
 let targetSum = 10;
 
 twoNumberSum(array, targetSum);
+
+//result = [11, -1] numbers can be in reverse order
