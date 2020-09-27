6ca0696cc1c9b097f1bf8fe891621971ccabe045
macandrew6 <mr.betterandrewmac@gmail.com>
2020-09-26 20:51:33 -0700

    pseudo codes optimal solution

--- two_number_sum.js
+++ two_number_sum.js
@@ -1,10 +1,17 @@
 function twoNumberSum(array, targetSum) {
+  // brute force method
   // Write your code here.
   // init an empty array
   // iterate through the array with i
   // make sure not to count the first character iterate through the array again with j
   // with and if condition save values if i + j === targetSum
   // make sure there are no duplicates
+  // efficient methods
+  // iterate once through the array and hold key value pairs
+  // if(pairs[targetSum - currentValue] !== null)
+  // save [i, pairs[targetSum - currentValue]]
+  // else
+  // save {i : targetSum - currentValue}
 }
 
 let array = [3, 5, -4, 8, 11, 1, -1, 6];
