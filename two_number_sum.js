dd2e26be5ea90c6f076930b9dc125975fd01ccc1
macandrew6 <mr.betterandrewmac@gmail.com>
2020-09-27 18:08:26 -0700

    debugging solution

--- two_number_sum.js
+++ two_number_sum.js
@@ -6,17 +6,26 @@ function twoNumberSum(array, targetSum) {
   // make sure not to count the first character iterate through the array again with j
   // with and if condition save values if i + j === targetSum
   // make sure there are no duplicates
-  // efficient methods
+  //  efficient methods
+  let prevValues = {};
   // iterate once through the array and hold key value pairs
-  // if(pairs[targetSum - currentValue] !== null)
-  // save [i, pairs[targetSum - currentValue]]
-  // else
-  // save {i : targetSum - currentValue}
+  array.forEach((curr, i) => {
+    // if(pairs[targetSum - currentValue] !== null)
+    let neededVal = targetSum - curr;
+    let secondVal = prevValues[neededVal];
+    if (secondVal !== null) {
+      // save [i, pairs[targetSum - currentValue]]
+      return [curr, secondVal];
+    } else {
+      // save {i : targetSum - currentValue}
+      prevValues[secondVal] = curr;
+    }
+  });
 }
 
 let array = [3, 5, -4, 8, 11, 1, -1, 6];
 let targetSum = 10;
 
-twoNumberSum(array, targetSum);
+console.log(twoNumberSum(array, targetSum));
 
 //result = [11, -1] numbers can be in reverse order
