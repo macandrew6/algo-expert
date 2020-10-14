299fcd77eed1b363562fd7169f31a1a336524de1
macandrew6 <mr.betterandrewmac@gmail.com>
2020-10-13 22:22:00 -0700

    init closest value var

--- closest_val_in_BST.js
+++ closest_val_in_BST.js
@@ -71,6 +71,7 @@ function findClosestValueInBst(bst, target) {
   // new pseudo code
   // start with conditions
   let currentNode = bst;
+  let closestValue = bst.value;
 }
 
 let bst = new BST(10);
