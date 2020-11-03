70ef74d0b623aefe9f32aed80d6391938e056ed5
macandrew6 <mr.betterandrewmac@gmail.com>
2020-11-02 23:21:46 -0800

    pseudo coding branch sum

--- branch_sums.js
+++ branch_sums.js
@@ -7,4 +7,8 @@ branch sum
 A branch sum is the sum of all values in a Binary Tree branch. A
 Binary Tree branch is a path of nodes in a tree that starts at the
 root node and ends at any leaf node.
+
+Each BinaryTree node has an interger value, a left child node,
+and a right child node. Children nodes can either be BinaryTree 
+nodes themselves or None/null.
  */
