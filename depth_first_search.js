c718b5871191fdfe00e9e2e364fa71dade733ee9
macandrew6 <mr.betterandrewmac@gmail.com>
2020-11-18 00:03:01 -0800

    defines depth first search problem

---
+++ depth_first_search.js
@@ -0,0 +1,12 @@
+/*
+Depth-first Search
+
+You're given a Node class that has a name and an array of optional 
+children nodes. When put together, nodes form an acyclic tree-like 
+structure.
+
+Implement the depthFirstSearch method on the Node class, which
+takes in an empty array, traverses the tree using the Depth-first 
+Search approach (specifically navigating the tree from left to right),
+stores all the nodes' names in the input array, and returns it.
+*/
