d5777715f2d472dbc80265f31b58f167999c7198
macandrew6 <mr.betterandrewmac@gmail.com>
2021-01-12 17:07:40 -0800

    optimize insert fn

--- linked-list-construction.js
+++ linked-list-construction.js
@@ -111,6 +111,7 @@ class DoublyLinkedList {
   // O (N) time | O(1) space
   insertAtPosition(position, nodeToInsert) {
     // Write your code here.
+    // bug in this fn
     if (position === 1) {
       this.setHead(nodeToInsert);
     }
