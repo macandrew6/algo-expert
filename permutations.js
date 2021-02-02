553e282a8ffb2adf6a20b49416ce913d9d7daa29
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-01 23:34:28 -0800

    init permutations

---
+++ permutations.js
@@ -0,0 +1,16 @@
+/*
+Permutations
+
+Write a function that takes in an array of unique integers and returns an 
+array of all permutations of those integers in no particular order.
+
+If the input array is empty, the function should return an empty array
+
+sample:
+array = [1, 2, 3]
+[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+*/
+
+function getPermutations(array) {
+  // Write your code here.
+}
