9a394081e965dbed34b421838f0704560d1db44f
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-03 18:17:58 -0800

    pseudo code 2nd solution

--- permutations.js
+++ permutations.js
@@ -16,22 +16,36 @@ function getPermutations(array) {
   // Write your code here.
 }
 
-function helper(array, perm, perms) {
-  let newArr;
-  let newPerm;
-  if (array.length === 0) {
-    perms.push(perm);
-  } else {
-    for (let i = 0; i < array.length; i++) {
-      let num = array[i];
-      newArr = removeNumFrom(array);
-      newPerm = perm + num;
-      helper(newArr, newPerm, perms)
-    }
-  }
-}
+// first solution
 
-function removeNumFrom(arr) {
-  arr.shift()
-  return arr;
-}
+// function helper(array, perm, perms) {
+//   let newArr;
+//   let newPerm;
+//   if (array.length === 0) {
+//     perms.push(perm);
+//   } else {
+//     for (let i = 0; i < array.length; i++) {
+//       let num = array[i];
+//       newArr = removeNumFrom(array);
+//       newPerm = perm + num;
+//       helper(newArr, newPerm, perms)
+//     }
+//   }
+// }
+
+// function removeNumFrom(arr) {
+//   arr.shift()
+//   return arr;
+// }
+
+// second solution
+
+/*
+[1, 2, 3] -> [1, 3, 2] -> [1, 2, 3]* -> [2, 1, 3] -> [2, 3, 1] -> [2, 1, 3] -> [1, 2, 3]* ->
+[3, 2, 1] -> [3, 1, 2] -> [1, 2, 3]*
+swapping strategy
+put a pointer at the first number
+put a second pointer at the second number
+put a third pointer at the third number
+
+*/
