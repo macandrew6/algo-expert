b9c88ef2fcc4e3693ce6cc6c8cbeccfee9cf36de
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-05 13:08:07 -0800

    init powerset

---
+++ powerset.js
@@ -0,0 +1,14 @@
+/*
+Powerset
+
+Wirte a function that takes in an array of unique integers and returns its powerset.
+
+The powerset P(X) of a set X is the set of all subsets of X. For example, the powerset of 
+[1, 2] is [[], [1], [2], [1, 2]]
+
+Example:
+let array = [1, 2, 3];
+=> [[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
+*/
+
+const powerset = (array) => {};
