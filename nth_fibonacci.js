585e70bb09dee6949b12be30f3e4d84d7187bb11
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-07 15:30:03 -0800

    init nth fibonacci

---
+++ nth_fibonacci.js
@@ -0,0 +1,20 @@
+/*
+Nth Fibonacci
+
+The Fibonacci sequence is defined as follows: the first number of the
+sequence is 0, the second number is 1, and the nth number is the sum
+of the (n-1)th and (n-2)th numbers. Write a function that takes in an
+integer n and returns the nth fibonacci number.
+
+Important note: the Fibonacci sequence is often define with its first
+two numbers as F0 = 0 and F1 = 1. For the purpose of this question, the 
+first Fibonacci number is F0; therefore, genNthFib(1) is equal to f0, 
+getNthFib(2) is equal to F1, etc...
+
+example:
+n = 2;
+1 => 0, 1
+
+n = 6;
+5 => 0, 1, 1, 2, 3, 5
+*/
