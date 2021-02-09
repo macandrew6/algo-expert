7f7967aac93a482c9cd7ffcb3519dda54e909ea8
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-08 23:36:51 -0800

    solves findThreeLargest

--- findThreeLargestNumbers.js
+++ findThreeLargestNumbers.js
@@ -24,7 +24,21 @@ const findThreeLargestNumbers = (array) => {
 
 const updateLargest = (threeLargest, num) => {
   if (threeLargest[2] === null || num > threeLargest[2]) {
-    shiftAndUpdate(); // todo
+    shiftAndUpdate(threeLargest, num, 2);
+  } else if (threeLargest[1] === null || num > threeLargest[1]) {
+    shiftAndUpdate(threeLargest, num, 1);
+  } else if (threeLargest[0] === null || num > threeLargest[0]) {
+    shiftAndUpdate(threeLargest, num, 0);
+  }
+};
+
+const shiftAndUpdate = (array, num, idx) => {
+  for (let i = 0; i <= idx; i++) {
+    if (i === idx) {
+      array[i] = num;
+    } else {
+      array[i] = array[i + 1];
+    }
   }
 };
 
