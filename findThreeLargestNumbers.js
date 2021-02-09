9aee696ff6875bbee71855aef6d029ca3cfc9bd9
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-08 23:05:05 -0800

    adds interview question

---
+++ findThreeLargestNumbers.js
@@ -0,0 +1,21 @@
+/*
+Find Three LargestNumbers
+
+Write a function that takes in an array of at least three integers and, 
+without sorting the input array, returns the sorted array of the three 
+largest integers in the input array
+
+example:
+array = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
+output => [18, 141, 541]
+*/
+
+const findThreeLargestNumbers = (array) => {
+  //init [null, null, null];
+  //keep track and store the three largest numbers
+  //update
+};
+
+console.log(
+  findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7])
+);
---
+++ transform.js
@@ -0,0 +1,33 @@
+/*
+ Given an input array, return a new array that maps each initial value with its corresponding ordered index (ascending)
+*/
+
+function transform(arr) {
+  const sorted = [...arr].sort((a, b) => a - b);
+  const hash = {};
+  const result = [];
+
+  // [1, 2, 3, 5, 5]
+
+  sorted.forEach((el, i) => {
+    if (hash[el]) {
+      hash[el].push(i);
+    } else {
+      hash[el] = [i];
+    }
+  });
+
+  // hash = {1: [0], 2: [1], 3: [2], 5: [3, 4]}
+
+  arr.forEach((el) => {
+    result.push(hash[el].pop());
+  });
+
+  return result;
+}
+
+/* [5, 10, 15, 20, 35] */
+
+console.log(transform([20, 10, 15, 35, 5])); // (Array)  [3, 1, 2, 4, 0]
+console.log(transform([1, 2, 3, 5, 4])); // (Array)  [0, 1, 2, 4, 3]
+console.log(transform([1, 2, 3, 5, 5]));
