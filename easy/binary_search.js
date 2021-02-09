e66290768dd6954c3b48b836df4709e83905ec80
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-09 14:55:32 -0800

    init selection sort

---
+++ easy/binary_search.js
@@ -0,0 +1,38 @@
+/*
+Binary Search
+
+Write a function that take in a sorted array of integers as well as a target
+integer. The function should use a Binary Search algorithm to determine if the 
+target integer is contained in the array and should return its index if it is,
+otherwise -1
+
+Example:
+
+array = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73] (sorted)
+target = 33
+returns => 3 (index)
+*/
+
+// time O (log(n)) | space O(log(n))
+const binarySearch = (array, target) => {
+  // left pointer
+  // right pointer
+  return helper(array, target, 0, array.length - 1);
+};
+
+const helper = (array, target, left, right) => {
+  if (left > right) {
+    return -1;
+  }
+  let midPoint = Math.floor((right + left) / 2);
+  let match = array[midPoint];
+  if (target > match) {
+    return helper(array, target, midPoint + 1, right);
+  } else if (target < match) {
+    return helper(array, target, left, midPoint - 1);
+  } else {
+    return midPoint;
+  }
+};
+
+console.log(binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33));
---
+++ easy/selection_sort.js
@@ -0,0 +1,43 @@
+/*
+Selection Sort
+
+Write a function that takes in an array of integers and returns a sorted
+version of that array. Use the Selection Sort alorithm to sort the array.
+
+array = [8, 5, 2, 9, 5, 6, 3]
+[2, 3, 5, 5, 6, 8, 9]
+
+not the most performant
+happens in place in the array
+dividing it into two seperate lists within that list
+one sublist with sorted numbers
+one sublist with unsorted numbers
+
+iterate through the unsorted list numerous times
+*/
+
+// time = O (n^2) | space O (1)
+// array = [8, 5, 2, 9, 5, 6, 3]
+// smallIdx = 6
+// currentIdx = 2
+// i = 6
+// array[i] = 9
+// array = [2, 5, 8, 9, 5, 6, 3]
+// array = [8, 3, 2, 9, 5, 6, 5]
+
+const selectionSort = (array) => {
+  let currentIdx = 0;
+  while (currentIdx < array.length - 1) {
+    let smallestIdx = currentIdx;
+    for (let i = currentIdx + 1; i < array.length; i++) {
+      if (array[i] < array[smallestIdx]) smallestIdx = i;
+    }
+    swap(smallestIdx, currentIdx, array);
+    currentIdx += 1;
+  }
+  return array;
+};
+
+const swap = (idx1, idx2, array) => {
+  [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+};
