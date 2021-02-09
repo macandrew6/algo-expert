e029e988b8f89cbd1b9dfcd12ea9d362af74003c
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-09 12:46:29 -0800

    solves bubble sort

--- easy/bubble_sort.js
+++ easy/bubble_sort.js
@@ -6,6 +6,34 @@ of that array. Use the Bubble Sort algorithm to sort the array.
 
 If you're unfamiliar with Bubble Sor, we recommend watching the Conceptual overview
 section of this question's video explanation before starting to code.
+
+example:
+[8, 5, 2, 9, 5, 6, 3]
+ <->
+[5, 8, 2, 9, 5, 6, 3]
+    <->
+[5, 2, 8, 9, 5, 6, 3]
+....
 */
 
-const bubbleSort = (array) => {};
+const bubbleSort = (array) => {
+  // traverse the array
+  let isSorted = false;
+  // if el1 is greater than el2
+  // we swap them
+  // check to see if there were any swaps stop running if there are no swaps
+  while (!isSorted) {
+    isSorted = true;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] >= array[i + 1]) {
+        swap(i, i + 1, array);
+        isSorted = false;
+      }
+    }
+  }
+  return array;
+};
+
+const swap = (idx1, idx2, array) => {
+  [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+};
