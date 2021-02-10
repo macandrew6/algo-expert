3a7cfac2ae56f35e9aedde0f844d7714dd58ac67
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-09 20:08:15 -0800

    init insertion sort

---
+++ easy/insertion_sort.js
@@ -0,0 +1,15 @@
+/*
+Insertion Sort
+
+Write a function that takes in an array of integers and returns a sorted version of
+that array. Use the Insertion Sort algorithm to sort the array.
+
+example:
+array = [8, 5, 2, 9, 5, 6, 3]
+output => [2, 3, 5, 5, 6, 8, 9]
+
+not the most performant sorting algo,
+but easy to understand and easy to implement
+*/
+
+const insertionSort = (array) => {};
