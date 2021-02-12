a0544a825dee271d242385c6e239abb4b672ad50
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-12 12:11:20 -0800

    testing

--- easy/throttle.js
+++ easy/throttle.js
@@ -5,6 +5,18 @@ write your own throttle f(n). Throttle executes your call back and stops
 further execution until the end of the duration of the time input.
 */
 
-const myThrottle = (interval) => {
-  return;
+Function.prototype.myThrottle = (interval) => {
+  let tooSoon = false;
+  return (...args) => {
+    if (!tooSoon) {
+      tooSoon = true;
+      setTimeout(() => (tooSoon = false), interval);
+      this(...args);
+    }
+  };
 };
+
+let fn = (a, b) => a + b;
+console.log(fn.myThrottle(50000));
+console.log(fn(1, 2));
+console.log(fn(1, 2));
