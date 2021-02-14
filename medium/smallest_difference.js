a5fd48ed0e3c4ec8da56ccd540b1de4b8b3e76fe
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-14 14:45:06 -0800

    completes smallest difference

--- medium/smallest_difference.js
+++ medium/smallest_difference.js
@@ -19,10 +19,50 @@ result = [28, 26]
 
 /*
 (both sorted)
- V
-[26, 134, 135]
-[10, 20, 28, 300]
-  ^
-init smallestDifference to Infinity;
-
+            V
+[10, 20, 28] = x
+[15, 17, 26, 134, 135] = y
+              ^
+init smallestDifference = 5 -> 3 -> 2
+currentDifference = Math.abs(x[0] - y[0]) = 2
+currentDiffernece < smallestDifference
+x[i] < y[i] move x pointer
+x[i] > y[i] move y pointer
 */
+
+const smallestDifference = (arrayOne, arrayTwo) => {
+  arrayOne.sort((a, b) => a - b);
+  arrayTwo.sort((a, b) => a - b);
+  let hash = {};
+  let smallestDifference = Math.abs(arrayOne[0] - arrayTwo[0]);
+  let arrayOneIdx = 0;
+  let arrayTwoIdx = 0;
+  while (arrayOneIdx < arrayOne.length && arrayTwoIdx < arrayTwo.length) {
+    let currentDifference = Math.abs(
+      arrayOne[arrayOneIdx] - arrayTwo[arrayTwoIdx]
+    );
+    if (currentDifference < smallestDifference) {
+      smallestDifference = currentDifference;
+      hash[smallestDifference] = [arrayOne[arrayOneIdx], arrayTwo[arrayTwoIdx]];
+    }
+    if (arrayOne[arrayOneIdx] < arrayTwo[arrayTwoIdx]) {
+      arrayOneIdx++;
+    } else if (arrayOne[arrayOneIdx] > arrayTwo[arrayTwoIdx]) {
+      arrayTwoIdx++;
+    } else {
+      return [arrayOne[arrayOneIdx], arrayTwo[arrayTwoIdx]];
+    }
+  }
+  let smallestKey = Infinity;
+  for (let key in hash) {
+    if (key < smallestKey) {
+      smallestKey = Number(key);
+    }
+  }
+  return hash[smallestKey];
+};
+
+let arrayOne = [-1, 5, 10, 20, 28, 3];
+let arrayTwo = [26, 134, 135, 15, 17];
+
+console.log(smallestDifference(arrayOne, arrayTwo));
