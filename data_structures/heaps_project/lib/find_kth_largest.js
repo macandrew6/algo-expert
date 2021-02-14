d1ed465895167b5a53a68e43f54ac8a70a20a7c5
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-14 15:57:21 -0800

    adds kth largest of unsorted array

---
+++ data_structures/heaps_project/lib/find_kth_largest.js
@@ -0,0 +1,13 @@
+/*
+Kth Largest Element in an Array
+
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in
+the sorted order, not the kth distinct element.
+
+Example:
+input: [3, 2, 1, 5, 6, 4] and k = 2;
+output: 5
+
+input: [3, 2, 3, 1, 2, 4, 5, 5, 6] and k = 4;
+output: 4
+*/
