de111c7602c93e373407c45c981cf1e9cccfc3c5
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-16 04:32:05 -0800

    solves spiral traverse iter

--- medium/spiral_traverse.js
+++ medium/spiral_traverse.js
@@ -33,13 +33,39 @@ height = array.length;
 
 */
 
-const spiralTraverse = (array) => {};
+const spiralTraverse = (array) => {
+  let result = [];
+  let startCol = 0;
+  let endCol = array[0].length - 1;
+  let startRow = 0;
+  let endRow = array.length - 1;
+  while (startCol <= endCol && startRow <= endRow) {
+    for (let col = startCol; col < endCol + 1; col++) {
+      result.push(array[startRow][col]);
+    }
+    for (let row = startRow + 1; row < endRow + 1; row++) {
+      result.push(array[row][endCol]);
+    }
+    for (let col = endCol - 1; col >= startCol; col--) {
+      result.push(array[endRow][col]);
+    }
+    for (let row = endRow - 1; row > startRow; row--) {
+      result.push(array[row][startCol]);
+    }
+    startRow++;
+    endRow--;
+    startCol++;
+    endCol--;
+  }
+  return result;
+};
 /*
 array = [
-  [1,   2,  3, 4],
-  [12, 13, 14, 5],
-  [11, 16, 15, 6],
-  [10,  9,  8, 7]
+    sc          ec
+sr  [1,   2,  3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+er  [10,  9,  8, 7]
                ^
 ]
 */
