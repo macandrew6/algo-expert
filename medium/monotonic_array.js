55ba273cf090d5f636e2288170ea9d7677ef58d8
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-15 23:38:59 -0800

    debuggs isMonotonic

--- medium/monotonic_array.js
+++ medium/monotonic_array.js
@@ -15,4 +15,20 @@ Note that empty arrays and arrays of one element are monotonic.
 
 example:
 array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+                 V   V
+[-1, -5, -10, -1100, -1100, -1101, -1102, -9001]
+direction = 1, 0, -1
+currentDirection = el
+next = el @ i + 1
 */
+
+const isMonotonic = (array) => {
+  let direction = 0;
+  for (let i = 0; i < array.length; i++) {
+    let current = array[i];
+    let next = array[i + 1];
+  }
+};
+
+let array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
+console.log(isMonotonic(array));
