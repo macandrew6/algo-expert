50bdfd9fa47e82ad520924ab544cd8a25995d091
macandrew6 <mr.betterandrewmac@gmail.com>
2021-02-17 22:34:37 -0800

    init array of products

---
+++ medium/array_of_products.js
@@ -0,0 +1,7 @@
+/*
+Array of Products
+
+Write a function that takes in a non-empty array of integers and returns an array
+of the same length, where each element in the output array is equal to the product
+of every other number
+*/
