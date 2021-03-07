cf164fefebdf29d03e27ad7d5c9d1bcdd4d47ade
macandrew6 <mr.betterandrewmac@gmail.com>
2021-03-06 20:43:34 -0800

    init generate document

---
+++ easy/generate_document.js
@@ -0,0 +1,44 @@
+/*
+Generate Document
+
+You're given a string of available characters and a string representing a document that you need to
+generate. Write a function that determines if you can generate the document using the available 
+characters. If you can generate the document, your function should return true; otherwise, it
+should return false.
+
+You're only able to generate the documents if the frequency of unique haracters in the characters
+string is greater than or equal to the frequency of unique characters in the document string. For
+example, if you're given characters = "abcabc" and the document = "aabbccc" you cannot generate
+the document because you're missing one "c".
+
+The document that you need to create may contain any characers, including special characters, 
+capital letters, numbers, and spaces.
+
+Example:
+input:
+characters = "Bste!hetsi ogEAxpelrt x "
+document = "AlgoExpert is the Best!"
+
+output:
+true
+*/
+
+const generateDocument = (characters, document) => {
+  let characterHash = {};
+  for (let i = 0; i < characters.length; i++) {
+    if (!characterHash[characters[i]]) {
+      characterHash[characters[i]] = [];
+    }
+    characterHash[characters[i]].push(characters[i]);
+  }
+  console.log(characterHash);
+  for (let i = 0; i < document.length; i++) {
+    if (characterHash[document[i]]) {
+      characterHash[document[i]].shift();
+    }
+  }
+};
+
+let characters = 'Bste!hetsi ogEAxpelrt x ';
+let document = 'AlgoExpert is the Best!';
+console.log(generateDocument(characters, document));
