71056a287ea821ccfc75a921faebe547554dd3c1
macandrew6 <mr.betterandrewmac@gmail.com>
2021-03-16 00:23:49 -0700

    pseudo code

--- medium/kadanes_algorithm.js
+++ medium/kadanes_algorithm.js
@@ -1,5 +1,5 @@
 /*
-Kadane's Algrithm
+Kadane's Algrithm (solves the maximum subarray problem)
 
 Write a function that takes in a non-empty array of integers and returns the maximum sum
 that can be obtained by summing up all of the integers in a non-empty sunarray of the input
@@ -12,4 +12,16 @@ array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
 
 output:
 19 // [1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1]
+
+scratchpad:
+[3, -10, 4] => 4 // [4]
+[3, -2, 4] => 5 // [3, -2, 4]
+
 */
+
+const kadanesAlgorithm = (array) => {
+  // formula: max(sum + curr , curr)
+};
+
+let array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4];
+console.log(kadanesAlgorithm(array));
